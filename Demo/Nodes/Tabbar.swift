517355a46c11dc1a25a25e2f409f30e26988e682
Luca Querella <luca.querella@gmail.com>
2016-08-16 13:43:33 +0200

    Improve Demo (create Album)

--- Demo/Nodes/Tabbar.swift
+++ Demo/Nodes/Tabbar.swift
@@ -9,85 +9,101 @@
 import Katana
 
 struct TabbarProps : Equatable,Frameable {
-  var frame = CGRect.zero
-  
-  static func ==(lhs: TabbarProps, rhs: TabbarProps) -> Bool {
-    return lhs.frame == rhs.frame
-  }
-  
+    var frame = CGRect.zero
+    
+    static func ==(lhs: TabbarProps, rhs: TabbarProps) -> Bool {
+        return lhs.frame == rhs.frame
+    }
+    
 }
 
 struct TabbarState : Equatable {
-  
-  static func ==(lhs: TabbarState, rhs: TabbarState) -> Bool {
-    return false
-  }
-  
+    
+    var section : Int
+    
+    static func ==(lhs: TabbarState, rhs: TabbarState) -> Bool {
+        return false
+    }
+    
 }
 
 struct Tabbar : NodeDescription {
-  
-  var props : TabbarProps
-  var children: [AnyNodeDescription] = []
-  
-  static var initialState = TabbarState()
-  static var viewType = UIView.self
-  
-  
-  
-  static func render(props: TabbarProps,
-                     state: TabbarState,
-                     children: [AnyNodeDescription],
-                     update: (TabbarState)->()) -> [AnyNodeDescription] {
+    
+    var props : TabbarProps
+    var children: [AnyNodeDescription] = []
+    
+    static var initialState = TabbarState(section: 0)
+    static var viewType = UIView.self
     
     
-    struct Section {
-      var color: UIColor
-      var node: AnyNodeDescription
+    
+    static func render(props: TabbarProps,
+        state: TabbarState,
+        children: [AnyNodeDescription],
+        update: (TabbarState)->()) -> [AnyNodeDescription] {
+        
+        
+        struct Section {
+            var color: UIColor
+            var node: AnyNodeDescription
+        }
+        
+        let sections = [
+            Section(
+                color: .red,
+                node: Album(props: AlbumProps()
+                    .frame(props.frame.size))
+            ),
+            
+            Section(
+                color: .orange,
+                node: View(props: ViewProps()
+                    .frame(props.frame.size)
+                    .color(.orange))
+            ),
+            
+            Section(
+                color: .green,
+                node: View(props: ViewProps()
+                    .frame(props.frame.size)
+                    .color(.green))
+            ),
+            
+            Section(
+                color: .white,
+                node: View(props: ViewProps()
+                    .frame(props.frame.size)
+                    .color(.white))
+            ),
+            
+            Section(
+                color: .purple,
+                node: View(props: ViewProps()
+                    .frame(props.frame.size)
+                    .color(.purple))
+            )
+        ]
+        
+        return [
+            
+            sections[state.section].node,
+            
+            View(props: ViewProps().frame(0,435,320,45).color(.black), children: sections.enumerated().map { (index,section) in
+                
+                let width = props.frame.size.width/CGFloat(sections.count)
+                let frame = CGRect(x: width * CGFloat(index), y: 0, width: width, height: 45)
+                
+                return View(props: ViewProps().color(.black).frame(frame), children: [
+                    Button(props: ButtonProps()
+                        .frame(10,10,width-20,45-20)
+                        .onTap { update(TabbarState(section: index)) }
+                        .color(section.color))
+                ])
+            })
+        ]
     }
     
-    let sections = [
-      Section(
-        color: .red,
-        node: View(props: ViewProps().color(.red))
-      ),
-      
-      Section(
-        color: .orange,
-        node: View(props: ViewProps().color(.orange))
-      ),
-      
-      Section(
-        color: .green,
-        node: View(props: ViewProps().color(.green))
-      ),
-      
-      Section(
-        color: .white,
-        node: View(props: ViewProps().color(.white))
-      ),
-      
-      Section(
-        color: .purple,
-        node: View(props: ViewProps().color(.purple))
-      )
-    ]
-    
-    return [
-      View(props: ViewProps().frame(0,0,320,435).color(.blue)),
-      View(props: ViewProps().frame(0,435,320,45).color(.black), children: sections.enumerated().map { (index,section) in
-        
-        let width = props.frame.size.width/CGFloat(sections.count)
-        let frame = CGRect(x: width * CGFloat(index), y: 0, width: width, height: 45)
-        
-        return View(props: ViewProps().color(.black).frame(frame), children: [
-          View(props: ViewProps().frame(10,10,width-20,45-20).color(section.color))
-          ])
-        })
-    ]
-  }
-  
-  init(props: TabbarProps) {
-    self.props = props
-  }
+    init(props: TabbarProps) {
+        self.props = props
+    }
 }
