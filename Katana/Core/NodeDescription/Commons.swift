bec3224af3a28d104538de92646dd2ee1b48c28a
Mauro Bolis <bolismauro@gmail.com>
2016-10-31 16:05:32 +0100

    Remove unused extensions and methods
    # Conflicts:
    #	Katana/Core/NodeDescription/Commons.swift

--- Katana/Core/NodeDescription/Commons.swift
+++ Katana/Core/NodeDescription/Commons.swift
@@ -10,40 +10,16 @@ import UIKit
 
 public protocol Frameable {
   var frame: CGRect {get set}
-  func frame(_ x: CGFloat, _ y: CGFloat, _ width: CGFloat, _ height: CGFloat) -> Self
-  func frame(_: CGRect) -> Self  
-}
-
-public extension Frameable {
-  func frame(_ x: CGFloat, _ y: CGFloat, _ width: CGFloat, _ height: CGFloat) -> Self {
-    var copy = self
-    copy.frame = CGRect(x: x, y: y, width: width, height: height)
-    return copy
-  }
-  
-  func frame(_ frame: CGRect) -> Self {
-    var copy = self
-    copy.frame = frame
-    return copy
-  }
-  
-  func frame(_ size: CGSize) -> Self {
-    var copy = self
-    copy.frame = CGRect(origin: CGPoint.zero, size: size)
-    return copy
-  }
 }
 
 public protocol Keyable {
   var key: String? { get set }
-  func key<Key>(_ key: Key) -> Self
+  mutating func setKey<K>(_ key: K)
 }
 
 public extension Keyable {
-  func key<Key>(_ key: Key) -> Self {
-    var copy = self
-    copy.key = "\(key)"
-    return copy
+  public mutating func setKey<Key>(_ key: Key) {
+    self.key = "\(key)"
   }
 }
 
@@ -52,14 +28,15 @@ public struct EmptyProps: NodeProps, Keyable {
   public var frame: CGRect = CGRect.zero
   
   public static func == (lhs: EmptyProps, rhs: EmptyProps) -> Bool {
-    return lhs.frame == rhs.frame
+    return
+      lhs.frame == rhs.frame &&
+      lhs.key == rhs.key
   }
   
   public init() {}
 }
 
 public struct EmptyState: NodeState {
-
   public static func == (lhs: EmptyState, rhs: EmptyState) -> Bool {
     return true
   }
