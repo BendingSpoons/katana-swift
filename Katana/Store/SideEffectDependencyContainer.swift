1501b98a402cd387b5893b38e5ce2a5c9622cfa6
Mauro Bolis <bolismauro@gmail.com>
2016-11-03 22:48:49 +0100

    Add documentation for side effects

--- Katana/Store/SideEffectDependencyContainer.swift
+++ Katana/Store/SideEffectDependencyContainer.swift
@@ -8,10 +8,29 @@
 
 import Foundation
 
+/**
+ Protocol that the side effect dependencies container should implement
+*/
 public protocol SideEffectDependencyContainer {
+  /**
+   Creates a new instance of the container.
+   A new container is created every time a side effect is invoked
+   
+   - parameter state:     the current state
+   - parameter dispatch:  a closure that can be used to dispatch actions
+   - returns: an instance of the container
+  */
   init(state: State, dispatch: @escaping StoreDispatch)
 }
 
+/// An empty dependencies container. It can be used for testing purposes or you don't need dependencies
 public struct EmptySideEffectDependencyContainer: SideEffectDependencyContainer {
+  /**
+   Creates the empty dependency container
+   
+   - parameter state:     the current state
+   - parameter dispatch:  a closure that can be used to dispatch actions
+   - returns: an instance of the container
+  */
   public init(state: State, dispatch: @escaping StoreDispatch) {}
 }
