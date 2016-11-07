42df9dd690a910d4ca16eea5d22e9b1b1a81aa3c
Alain Caltieri <alaincaltieri@gmail.com>
2016-11-07 17:29:08 +0100

    Connect the Katana root and add missing references to project

--- Examples/KatanaCodingLove/KatanaCodingLove/AppDelegate.swift
+++ Examples/KatanaCodingLove/KatanaCodingLove/AppDelegate.swift
@@ -13,35 +13,31 @@ import Katana
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+    var root: Root?
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]? = nil) -> Bool {
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = UIViewController()
+        self.window?.rootViewController?.view.backgroundColor = UIColor.white
+        self.window?.makeKeyAndVisible()
+        
+        
+        let view = (self.window?.rootViewController?.view)!
+        let rootBounds = UIScreen.main.bounds
+        
+        let store = Store<CodingLoveState>(middlewares: [], dependencies: EmptySideEffectDependencyContainer.self)
+        
+        self.root = CodingLove(props: CodingLove.Props.build({
+            $0.frame = rootBounds
+        })).makeRoot(store: store)
+            
+        self.root!.render(in: view)
+        
         return true
     }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
+    
 
 }
 
