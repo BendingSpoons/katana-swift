93c7f90f55c7d775b5af4ffa4ca1e0304123b1ff
Alain Caltieri <alaincaltieri@gmail.com>
2016-11-07 17:28:19 +0100

    Add Post plain model

---
+++ Examples/KatanaCodingLove/KatanaCodingLove/Models/Post.swift
@@ -0,0 +1,24 @@
+//
+//  Post.swift
+//  Katana
+//
+//  Created by Alain Caltieri on 07/11/16.
+//  Copyright © 2016 Bending Spoons. All rights reserved.
+//
+import UIKit
+
+struct Post {
+    let title: String
+    let image: UIImage
+    
+    init(title: String, image: UIImage) {
+        self.title = title
+        self.image = image
+    }
+    
+    static func == (lhs: Post, rhs: Post) -> Bool {
+        return lhs.title == rhs.title &&
+            lhs.image == rhs.image
+    }
+    
+}
