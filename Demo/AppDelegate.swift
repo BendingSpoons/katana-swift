1abe744ed0357f469a4ac1eec536e75a349883d7
Andrea De Angelis <smaramba83@gmail.com>
2016-11-09 15:18:13 +0100

    Include Demo
    Remove development team
    
    Include Demo
    
    Remove development team
    
    Include Demo

---
+++ Demo/AppDelegate.swift
@@ -0,0 +1,56 @@
+//
+//  AppDelegate.swift
+//  Demo
+//
+//  Created by Andrea De Angelis on 09/11/2016.
+//  Copyright © 2016 Bending Spoons. All rights reserved.
+//
+
+import Katana
+import KatanaElements
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  var renderer: Renderer?
+  
+  
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    self.window = UIWindow(frame: UIScreen.main.bounds)
+    self.window?.rootViewController = UIViewController()
+    self.window?.rootViewController?.view.backgroundColor = UIColor.white
+    self.window?.makeKeyAndVisible()
+    
+    counterDemo()
+    return true
+  }
+  
+  private func counterDemo() {
+    let view = (self.window?.rootViewController?.view)!
+    let rootBounds = UIScreen.main.bounds
+    
+    let store = Store<CounterState>()
+    let counterScreen = CounterScreen(props: CounterScreenProps.build({ (counterScreenProps) in
+      counterScreenProps.frame = rootBounds
+    }))
+    //self.connector = counterScreen.makeConnector(with: store)
+    //self.connector!.render(in: view)
+    
+    
+    
+    //different responsibilities for Nodes generation and Connector
+    //let renderer = Renderer(root: counterScreen)
+    //renderer.render(in: view)
+    //let connector = renderer.connectorForStore(store)
+    
+    
+    // Renderer proposal
+    renderer = Renderer(rootDescription: counterScreen, store: store)
+    //renderer.connect(to: store)
+    renderer!.render(in: view)
+  }
+  
+}
