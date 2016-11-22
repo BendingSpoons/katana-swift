536eef9a222d76f1f13946dab41ac739f8826c24
Andrea De Angelis <smaramba83@gmail.com>
2016-11-22 13:48:15 +0100

    Improve naming of the Frameworks

--- KatanaElements/Button.swift
+++ KatanaElements/Button.swift
@@ -63,10 +63,10 @@ public struct Button: NodeDescription {
     view.layer.borderColor = props.borderColor.cgColor
     view.clipsToBounds = props.clipsToBounds
     view.isEnabled = props.isEnabled
-    view.contentEdgeInsets = props.contentEdgeInsets.scale(by: node.plasticMultipler).toUIEdgeInsets
-    view.titleEdgeInsets = props.contentEdgeInsets.scale(by: node.plasticMultipler).toUIEdgeInsets
-    view.imageEdgeInsets = props.imageEdgeInsets.scale(by: node.plasticMultipler).toUIEdgeInsets
-    view.imageEdgeInsets = props.imageEdgeInsets.scale(by: node.plasticMultipler).toUIEdgeInsets
+    view.contentEdgeInsets = props.contentEdgeInsets.scale(by: node.plasticMultipler)
+    view.titleEdgeInsets = props.contentEdgeInsets.scale(by: node.plasticMultipler)
+    view.imageEdgeInsets = props.imageEdgeInsets.scale(by: node.plasticMultipler)
+    view.imageEdgeInsets = props.imageEdgeInsets.scale(by: node.plasticMultipler)
     view.adjustsImageWhenHighlighted = props.adjustsImageWhenHighlighted
     view.adjustsImageWhenDisabled = props.adjustsImageWhenDisabled
     view.showsTouchWhenHighlighted = props.showsTouchWhenHighlighted
