26509ef3d8900a7b83d7fab2e33eb2b9ae639348
Mauro Bolis <bolismauro@gmail.com>
2017-05-30 11:06:28 +0200

    Make animation options public

--- Katana/Core/Animations/AnimationType.swift
+++ Katana/Core/Animations/AnimationType.swift
@@ -29,43 +29,43 @@ public struct AnimationOptions: OptionSet {
   }
   
   /// Lay out subviews at commit time so that they are animated along with their parent.
-  static let layoutSubview = AnimationOptions(rawValue: 1 << 0)
+  public static let layoutSubview = AnimationOptions(rawValue: 1 << 0)
   
   /// Allow the user to interact with views while they are being animated.
-  static let allowUserInteraction = AnimationOptions(rawValue: 1 << 1)
+  public static let allowUserInteraction = AnimationOptions(rawValue: 1 << 1)
   
   /**
    Start the animation from the current setting associated with an already in-flight animation.
    If this key is not present, any in-flight animations are allowed to finish
    before the new animation is started. If another animation is not in flight, this key has no effect.
   */
-  static let beginFromCurrentState = AnimationOptions(rawValue: 1 << 2)
+  public static let beginFromCurrentState = AnimationOptions(rawValue: 1 << 2)
   
   /// Repeat the animation indefinitely.
-  static let repeatAnimation = AnimationOptions(rawValue: 1 << 3)
+  public static let repeatAnimation = AnimationOptions(rawValue: 1 << 3)
   
   /// Run the animation backwards and forwards. Must be combined with the `repeatAnimation` option.
-  static let autoreverse = AnimationOptions(rawValue: 1 << 4)
+  public static let autoreverse = AnimationOptions(rawValue: 1 << 4)
   
   /**
    Force the animation to use the original duration value specified when the
    animation was submitted. If this key is not present, the animation inherits the
    remaining duration of the in-flight animation, if any.
   */
-  static let overrideInheritedDuration = AnimationOptions(rawValue: 1 << 5)
+  public static let overrideInheritedDuration = AnimationOptions(rawValue: 1 << 5)
   
   /**
    Force the animation to use the original curve value specified when the animation
    was submitted. If this key is not present, the animation inherits the curve
    of the in-flight animation, if any.
   */
-  static let overrideInheritedCurve = AnimationOptions(rawValue: 1 << 6)
+  public static let overrideInheritedCurve = AnimationOptions(rawValue: 1 << 6)
   
   /**
    Animate the views by changing the property values dynamically and redrawing the view.
    If this key is not present, the views are animated using a snapshot image.
   */
-  static let allowAnimatedContent = AnimationOptions(rawValue: 1 << 7)
+  public static let allowAnimatedContent = AnimationOptions(rawValue: 1 << 7)
   
   /**
    When present, this key causes views to be hidden or shown (instead of removed or added)
@@ -74,62 +74,62 @@ public struct AnimationOptions: OptionSet {
    the to-view in a transition is added to, and the from-view is removed from,
    the parent view’s list of subviews.
   */
-  static let showHideTransitionViews = AnimationOptions(rawValue: 1 << 8)
+  public static let showHideTransitionViews = AnimationOptions(rawValue: 1 << 8)
   
   /// The option to not inherit the animation type or any options.
-  static let overrideInheritedOptions = AnimationOptions(rawValue: 1 << 9)
+  public static let overrideInheritedOptions = AnimationOptions(rawValue: 1 << 9)
   
   /**
    An ease-in ease-out curve causes the animation to begin slowly, accelerate
    through the middle of its duration, and then slow again before
    completing.
   */
-  static let curveEaseInOut = AnimationOptions(rawValue: 0 << 16)
+  public static let curveEaseInOut = AnimationOptions(rawValue: 0 << 16)
   
   /// An ease-in curve causes the animation to begin slowly, and then speed up as it progresses.
-  static let curveEaseIn = AnimationOptions(rawValue: 1 << 16)
+  public static let curveEaseIn = AnimationOptions(rawValue: 1 << 16)
   
   /// An ease-out curve causes the animation to begin quickly, and then slow as it completes.
-  static let curveEaseOut = AnimationOptions(rawValue: 2 << 16)
+  public static let curveEaseOut = AnimationOptions(rawValue: 2 << 16)
   
   /// A linear animation curve causes an animation to occur evenly over its duration.
-  static let curveLinear = AnimationOptions(rawValue: 3 << 16)
+  public static let curveLinear = AnimationOptions(rawValue: 3 << 16)
   
   /// No transition is specified.
-  static let transitionNone = AnimationOptions(rawValue: 0 << 20)
+  public static let transitionNone = AnimationOptions(rawValue: 0 << 20)
   
   /**
    A transition that flips a view around its vertical axis from left to right.
    The left side of the view moves toward the front and right side toward the back.
   */
-  static let transitionFlipFromLeft = AnimationOptions(rawValue: 1 << 20)
+  public static let transitionFlipFromLeft = AnimationOptions(rawValue: 1 << 20)
   
   /**
    A transition that flips a view around its vertical axis from right to left.
    The right side of the view moves toward the front and left side toward the back.
   */
-  static let transitionFlipFromRight = AnimationOptions(rawValue: 2 << 20)
+  public static let transitionFlipFromRight = AnimationOptions(rawValue: 2 << 20)
   
   /// A transition that curls a view up from the bottom.
-  static let transitionCurlUp = AnimationOptions(rawValue: 3 << 20)
+  public static let transitionCurlUp = AnimationOptions(rawValue: 3 << 20)
   
   /// A transition that curls a view down from the top.
-  static let transitionCurlDown = AnimationOptions(rawValue: 4 << 20)
+  public static let transitionCurlDown = AnimationOptions(rawValue: 4 << 20)
   
   /// A transition that dissolves from one view to the next.
-  static let transitionCrossDissolve = AnimationOptions(rawValue: 5 << 20)
+  public static let transitionCrossDissolve = AnimationOptions(rawValue: 5 << 20)
   
   /**
    A transition that flips a view around its horizontal axis from top to bottom.
    The top side of the view moves toward the front and the bottom side toward the back.
   */
-  static let transitionFlipFromTop = AnimationOptions(rawValue: 6 << 20)
+  public static let transitionFlipFromTop = AnimationOptions(rawValue: 6 << 20)
   
   /**
     A transition that flips a view around its horizontal axis from bottom to top.
    The bottom side of the view moves toward the front and the top side toward the back.
   */
-  static let transitionFlipFromBottom = AnimationOptions(rawValue: 7 << 20)
+  public static let transitionFlipFromBottom = AnimationOptions(rawValue: 7 << 20)
 }
 
 /// Enum that represents the animations that can be used to animate an UI update
