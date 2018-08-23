a8e70ac2911bc526635424f9cbbafe6d95ccf211
Alex Tosatto <at@bendingspoons.com>
2018-08-23 18:14:15 +0200

    Add action logger

---
+++ Katana/Middleware/ActionLogger/ActionLogger.swift
@@ -0,0 +1,44 @@
+//
+//  ActionLogger.swift
+//  Katana
+//
+//  Copyright © 2018 Bending Spoons.
+//  Distributed under the MIT License.
+//  See the LICENSE file for more information.
+//
+
+import Foundation
+
+/**
+ Struct that makes an action logger middleware available
+ */
+public struct ActionLogger {
+  
+  /**
+   This function returns a `StoreMiddleware` that intercepts and logs not black-listed actions
+   
+   - Parameters:
+       - blackList: list of action types that must not be logged
+   - Returns: store middleware that logs not black-listed actions
+   - See: `StoreMiddleware` for details
+   */
+  public static func middleware(blackList: [Action.Type] = []) -> StoreMiddleware {
+  
+    return { getState, dispatch in
+      return { next in
+        return { action in
+          
+          let previousState = getState()
+          next(action)
+          let currentState = getState()
+          
+          let actionType = type(of: action) as Action.Type
+          guard !blackList.contains(where: { $0 == actionType }) else { return }
+          
+          print("[Action]: \(action)")
+        }
+      }
+    }
+  }
+  
+}
