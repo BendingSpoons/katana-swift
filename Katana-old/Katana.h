0b396a2cf1efb723bebba1ec26e601ab118319f6
Mauro Bolis <bolismauro@gmail.com>
2018-10-18 16:13:23 +0200

    Prepare project for new release

---
+++ Katana-old/Katana.h
@@ -0,0 +1,19 @@
+//
+//  Katana.h
+//  Katana
+//
+//  Copyright © 2016 Bending Spoons.
+//  Distributed under the MIT License.
+//  See the LICENSE file for more information.
+
+@import UIKit;
+
+//! Project version number for Katana.
+FOUNDATION_EXPORT double KatanaVersionNumber;
+
+//! Project version string for Katana.
+FOUNDATION_EXPORT const unsigned char KatanaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Katana/PublicHeader.h>
+
+
