88c679b59babfd52e16830723626ea90adacd1fb
Mauro Bolis <bolismauro@gmail.com>
2018-10-25 10:34:15 +0200

    Move interceptor to proper file

---
+++ Katana/Interceptor/Interceptor.swift
@@ -0,0 +1,17 @@
+//
+//  Interceptor.swift
+//  Katana
+//
+//  Copyright © 2018 Bending Spoons.
+//  Distributed under the MIT License.
+//  See the LICENSE file for more information.
+//
+
+import Foundation
+
+public typealias StoreInterceptorNext = (_: Dispatchable) throws -> Void
+
+public typealias StoreInterceptor =
+  (_ getState: @escaping GetState, _ dispatch: @escaping PromisableStoreDispatch) ->
+  (_ next: @escaping StoreInterceptorNext) ->
+  (_ dispatchable: Dispatchable) throws -> ()
--- Katana/Types.swift
+++ Katana/Types.swift
@@ -14,13 +14,7 @@ public typealias StoreListener = () -> ()
 /// Typealias for the `Store` listener unsubscribe closure
 public typealias StoreUnsubscribe = () -> ()
 
-public typealias StoreInterceptorNext = (_: Dispatchable) throws -> Void
 public typealias GetState = () -> State
-
-public typealias StoreInterceptor =
-  (_ getState: @escaping GetState, _ dispatch: @escaping PromisableStoreDispatch) ->
-  (_ next: @escaping StoreInterceptorNext) ->
-  (_ dispatchable: Dispatchable) throws -> ()
 
 /// Typealias for the `Store` dispatch function with the ability of managing the output with a promise
 public typealias PromisableStoreDispatch = (_: Dispatchable) -> Promise<Void>
