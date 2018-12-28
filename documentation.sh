dcb3d38dfbc66b9a603d5f4c8b12bd0b01a7bc09
Mauro Bolis <bolismauro@gmail.com>
2018-12-28 19:07:45 +0100

    Change documentation theme

--- documentation.sh
+++ documentation.sh
@@ -3,4 +3,5 @@ jazzy \
   --author_url http://bendingspoons.com \
   --github_url https://github.com/BendingSpoons/katana-swift \
   --github-file-prefix https://github.com/BendingSpoons/katana-swift/blob/master/ \
-  --module Katana
+  --module Katana \
+  --theme fullwidth
