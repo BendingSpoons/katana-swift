37b338215695bff7ee8d04f51d3bf56a709068f0
Giovanni Filaferro <gif@bendingspoons.com>
2019-10-31 09:53:10 +0100

    fix: dependencies with https

--- Package.swift
+++ Package.swift
@@ -13,9 +13,9 @@ let package = Package(
     .library(name: "Katana", targets: ["Katana"]),
   ],
   dependencies: [
-    .package(url: "git@github.com:malcommac/Hydra.git", from: "1.2.1"),
-    .package(url: "git@github.com:Quick/Quick.git", from: "1.3.4"),
-    .package(url: "git@github.com:Quick/Nimble.git", from: "7.3.4")
+    .package(url: "https://github.com/malcommac/Hydra.git", from: "1.2.1"),
+    .package(url: "https://github.com/Quick/Quick.git", from: "1.3.4"),
+    .package(url: "https://github.com/Quick/Nimble.git", from: "7.3.4")
   ],
   targets: [
     .target(name: "Katana", dependencies: ["Hydra"], path: "Katana"),
