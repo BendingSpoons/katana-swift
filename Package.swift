7f4ef4dc129041b4c8bf5c32393fe8bd4637b659
Giovanni Filaferro <gif@bendingspoons.com>
2019-10-31 09:16:07 +0100

    feat: swift package manager compatibility

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "Katana",
+  platforms: [
+    .iOS(.v9),
+    .macOS(.v10_15)
+  ],
+  products: [
+    .library(name: "Katana", targets: ["Katana"]),
+  ],
+  dependencies: [
+    .package(url: "git@github.com:malcommac/Hydra.git", from: "1.2.1"),
+    .package(url: "git@github.com:Quick/Quick.git", from: "1.3.4"),
+    .package(url: "git@github.com:Quick/Nimble.git", from: "7.3.4")
+  ],
+  targets: [
+    .target(name: "Katana", dependencies: ["Hydra"], path: "Katana"),
+    .testTarget(name: "KatanaTests", dependencies: ["Katana", "Quick", "Nimble"], path: "KatanaTests")
+  ]
+)
